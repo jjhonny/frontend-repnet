7c7e0fab19ecd44b820e3d7dc90db613d19b2fd9
Jhonny Dias <ojhonny1@hotmail.com>
2024-03-19 13:22:31 -0400

    create project and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline flex justify-center items-center">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
