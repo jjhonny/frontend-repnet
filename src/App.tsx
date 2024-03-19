06aa399b35d4b7fe393637f19159721b46e90000
Jhonny Dias <ojhonny1@hotmail.com>
2024-03-19 13:37:10 -0400

    install component library `daisyUI`

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
       <h1 className="text-3xl font-bold underline flex justify-center items-center">
         Hello world!
       </h1>
+
+      <div className="flex justify-center items-center mt-5">
+        <button className="btn btn-primary">Teste</button>
+      </div>
     </>
   );
 }
