a4b0982b91a6e37b085d4530d1efcb77fffe1fe9
jjhonny <ojhonny1@hotmail.com>
2024-06-26 13:21:40 -0400

    feat: update

--- src/pages/representante/perfil/index.tsx
+++ src/pages/representante/perfil/index.tsx
@@ -55,7 +55,7 @@ export default function PerfilRepre() {
         <h1 className="text-2xl font-bold mb-4">
           Perfil do {user.categoria === "C" ? "Cliente" : "Representante"}
         </h1>
-        <div className="bg-white shadow-lg rounded-lg p-6">
+        <div className="bg-white shadow-2xl rounded-2xl p-6">
           <h2 className="text-xl font-semibold mb-4">Informações do Usuário</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid grid-cols-1 gap-4">
@@ -65,8 +65,9 @@ export default function PerfilRepre() {
                 </label>
                 <input
                   type="text"
-                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${editing ? "bg-white" : "bg-gray-100"
-                    }`}
+                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${
+                    editing ? "bg-white" : "bg-gray-100"
+                  }`}
                   {...register("razao_social", { required: true })}
                   readOnly={!editing}
                 />
@@ -89,8 +90,9 @@ export default function PerfilRepre() {
                 <label className="mb-2 font-medium text-gray-700">Email</label>
                 <input
                   type="email"
-                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${editing ? "bg-white" : "bg-gray-100"
-                    }`}
+                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${
+                    editing ? "bg-white" : "bg-gray-100"
+                  }`}
                   {...register("email", { required: true })}
                   readOnly={!editing}
                 />
@@ -117,8 +119,9 @@ export default function PerfilRepre() {
                 </label>
                 <input
                   type="password"
-                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${editing ? "bg-white" : "bg-gray-100"
-                    }`}
+                  className={`input input-bordered p-2 border rounded focus:outline-none focus:ring focus:border-blue-300 ${
+                    editing ? "bg-white" : "bg-gray-100"
+                  }`}
                   {...register("password")}
                   readOnly={!editing}
                 />
@@ -126,16 +129,22 @@ export default function PerfilRepre() {
             </div>
             <div className="mt-4">
               {!editing ? (
-                <button className="btn btn-primary" onClick={handleEditClick}>
+                <button
+                  className="btn btn-primary rounded-2xl"
+                  onClick={handleEditClick}
+                >
                   <FaPen /> Editar
                 </button>
               ) : (
                 <div>
-                  <button className="btn btn-primary mr-2" type="submit">
+                  <button
+                    className="btn btn-primary mr-2 rounded-2xl"
+                    type="submit"
+                  >
                     <FaCheck /> Salvar
                   </button>
                   <button
-                    className="btn btn-secondary"
+                    className="btn btn-secondary rounded-2xl"
                     onClick={() => setEditing(false)}
                   >
                     <FaTimes /> Cancelar
