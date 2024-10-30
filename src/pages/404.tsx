69b8167feefefdb9f016f808fa6723f9e9e25864
jjhonny <ojhonny1@hotmail.com>
2024-10-30 10:43:55 -0400

    fix: fix 404 page logic

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,25 @@
+import { UserProps } from "@/contexts/AuthContex";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
+  const [localUser, setLocalUser] = useState<UserProps | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setLocalUser(JSON.parse(storedUser));
+    }
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen justify-center items-center">
       <h2 className="font-bold text-2xl">404</h2>
       <p className="text-xl my-2">Está página não existe</p>
-      <Link className="btn btn-neutral" href="/produtos">
+      <Link
+        className="btn btn-neutral"
+        href={localUser?.categoria === "C" ? "/cliente" : "/representante"}
+      >
         Voltar para home
       </Link>
     </div>
