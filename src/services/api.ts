10029693dc50560a8e09935130829d285cb7b8e1
jjhonny <ojhonny1@hotmail.com>
2024-12-03 00:41:26 -0300

    feat: atualiza a URL base da API para o endpoint de produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ export function setupAPICliente(ctx = undefined): AxiosInstance {
 
   // Criando a instância do Axios
   const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "https://backend-pi-sigma.vercel.app",
     headers: {
       Authorization: `Bearer ${cookies["@nextauth.token"]}`,
       "Content-Type": "application/json",
